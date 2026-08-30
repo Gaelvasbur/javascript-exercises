58c525179141a96c074e8f7f52ad098b5e811d29
Gael Isaac Vásquez Burgos <gaelvasbur@gmail.com>
2026-08-29 20:48:18 -0600

    Update js_exercise6.js

--- js_exercise6.js
+++ js_exercise6.js
@@ -8,7 +8,6 @@ let minutes = 0;
 let hours = 0;
 let timerInterval = null;
 
-//funcion que indica como se vera el temporizador en pantalla
 function updateDisplay() {
     let s = seconds < 10 ? '0' + seconds : seconds;
     let m = minutes < 10 ? '0' + minutes : minutes;
@@ -18,9 +17,8 @@ function updateDisplay() {
 }
 
 startButton.addEventListener('click', () => {
-    if (timerInterval !== null) return; // si al presionar start ya habia empezado a contar lo paras yendo al return, y si no y es null, sigues con el codigo a timerInterval=1 por ej
+    if (timerInterval !== null) return; 
 
-    //Aqui setInterval retorna 1, realiza la cuenta que luego manda al visualizador div y con la regla de esperarse 1 segundo humano antes de ejecutar el codigo
     timerInterval = setInterval(() => {
         seconds++;
         
@@ -37,7 +35,7 @@ startButton.addEventListener('click', () => {
         updateDisplay();
     }, 1000);
 });
-//detiene nuestro proceso actual 1, con clearInterval(), y con timerInterval = null cambia el valor en memoria 1 por null
+
 pauseButton.addEventListener('click', () => {
     clearInterval(timerInterval);
     timerInterval = null;
